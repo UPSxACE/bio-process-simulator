5848dcc0b326a9e72e62e9186dde0de1b043b247
Eduardo Botelho <eduardo11224b@gmail.com>
2025-08-21 14:56:51 +0100

    initial setup

---
+++ src/main/java/com/upsxace/bio_process_simulator/BioProcessSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.upsxace.bio_process_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioProcessSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioProcessSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upsxace/bio_process_simulator/BioProcessSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upsxace.bio_process_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioProcessSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
